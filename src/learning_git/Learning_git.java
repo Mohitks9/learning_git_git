0f40aef7f6498bf4659ff8a07f6dbed5c5d2dc04
mohit <mohitkusahu9@gmail.com>
2022-10-22 23:03:31 +0530

    this is very first commit

---
+++ src/learning_git/Learning_git.java
@@ -0,0 +1,24 @@
+package learning_git;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Learning_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is working as Expected");
+		System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\mohit\\\\Kachra\\\\automationTesting\\\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\mohit\\\\Kachra\\\\automationTesting\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.udemy.com/course/selenium-real-time-examplesinterview-questions/learn/lecture/13861486#overview");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+		
+	}
+
+}
